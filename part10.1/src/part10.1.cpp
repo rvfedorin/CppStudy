3a074fbfe32c33f5f02e9aaee595508bd23b793c
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-12-01 16:46:43 +0300

    page 287 -> oneHour

---
+++ part10.1/src/part10.1.cpp
@@ -0,0 +1,59 @@
+//============================================================================
+// Name        : 1.cpp
+// Author      : Roman V. Fedorin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Fish{
+public:
+	bool isFreshWatreFish;
+
+	void Swim(){
+		if (isFreshWatreFish){
+			cout << "Пресноводный" << endl;
+		} else {
+			cout << "Морской" << endl;
+		}
+	}
+};
+
+class Carp: public Fish{
+public:
+	Carp(){
+		isFreshWatreFish = true;
+	}
+};
+
+class Tuna: public Fish{
+public:
+	Tuna(){
+		isFreshWatreFish = false;
+	}
+};
+
+int main() {
+	Carp myLanch;
+	Tuna myDinner;
+
+	cout << "Моя еда:" << endl;
+	cout << "Обед: ";
+	myLanch.Swim();
+	cout << "Ужин: ";
+	myDinner.Swim();
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
