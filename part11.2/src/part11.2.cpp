fb2f24c509a59fe0c51cfc5a79a0084ca181a8fa
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-12-09 13:35:32 +0300

    Clone method

--- part11.2/src/part11.2.cpp
+++ part11.2/src/part11.2.cpp
@@ -20,6 +20,8 @@ public:
 		cout << "Удаляем Fish " << endl;
 	}
 
+	virtual Fish* Clone() = 0;
+
 //	virtual void Swim(){
 //		cout << "Рыба плавает." << endl;
 //	}
@@ -32,11 +34,19 @@ public:
 	void Swim() override final {
 		cout << "Карп плавает медленно." << endl;
 	}
+
+	Fish* Clone() override {
+		return new Carp(*this);
+	}
 };
 
 class Tuna: public Fish{
 	void Swim(){
 		cout << "Тунец плавает быстро." << endl;
+	}
+
+	Fish* Clone() override {
+		return new Tuna(*this);
 	}
 };
 
@@ -45,11 +55,33 @@ void MakeFishSwim(Fish& fish){
 }
 
 int main() {
+	int const ARRAY_SIZE = 3;
 	Tuna tuna;
 	Carp carp;
 
 	MakeFishSwim(tuna);
 	MakeFishSwim(carp);
+
+	Fish* myFishes[ARRAY_SIZE] = {nullptr};
+	myFishes[0] = new Tuna;
+	myFishes[1] = new Tuna;
+	myFishes[2] = new Carp;
+
+	Fish* myNewFishes[ARRAY_SIZE];
+	for (int i=0; i < ARRAY_SIZE; ++i){
+		myNewFishes[i] = myFishes[i]->Clone();
+	}
+
+	// Проверка
+	for (int f=0; f<ARRAY_SIZE; ++f){
+		myNewFishes[f]->Swim();
+	}
+
+	// Удаляем
+	for (int index=0; index<ARRAY_SIZE; ++index){
+		delete myFishes[index];
+		delete myNewFishes[index];
+	}
 
 	return 0;
 }
